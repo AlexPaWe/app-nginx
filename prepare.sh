93f364bfab9be55e11c0d473772911d31ea778a9
AlexPaWe <alex-w93@gmx.de>
2021-10-12 13:32:05 +0200

    Fixed a bug where the configuration file was not found.

--- prepare.sh
+++ prepare.sh
@@ -4,7 +4,7 @@ ACCESS_LOG=y
 KEEPALIVE_TIMEOUT=10
 PAYLOAD_SIZE=500
 
-cat /nginx-$OPEN_FILE_CACHE.conf | sed "s/\$WORKER_CONNECTIONS/${WORKER_CONNECTIONS}/g" > ./fs0/nginx/conf/nginx.conf
+cat nginx-$OPEN_FILE_CACHE.conf | sed "s/\$WORKER_CONNECTIONS/${WORKER_CONNECTIONS}/g" > ./fs0/nginx/conf/nginx.conf
 if [[ $ACCESS_LOG == "y" ]]; then \
   export ACCESS_LOG="\/dev\/stdout"; \
 else \
@@ -16,4 +16,4 @@ sed -i "s/\$KEEPALIVE_TIMEOUT/${KEEPALIVE_TIMEOUT}/g" ./fs0/nginx/conf/nginx.con
 # Generate cpio image
 cd fs0/
 tr -dc A-Za-z0-9 </dev/urandom | head -c ${PAYLOAD_SIZE} > ./nginx/html/payload.txt
-find -depth -print | tac | bsdcpio -o --format newc > ../initramfs.cpio
+find -depth -print | tac | bsdcpio -o -v --format newc > ../initramfs.cpio
