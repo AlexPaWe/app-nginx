4a4f7ee00495bd54fc571620c91fa9c4f0c81096
AlexPaWe <alex-w93@gmx.de>
2021-10-15 15:44:52 +0200

    Fixed the location of the unikernel in the launch script.

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sudo qemu-system-x86_64 -cpu host -m 1G -nodefaults -display none -serial stdio -kernel build/app-nginx_kvm-x86_64 -initrd initramfs.cpio -append "-- -c /nginx/conf/nginx.conf" --enable-kvm -netdev user,id=n0,hostfwd=tcp::80-:80 -device virtio-net,netdev=n0 -s
+sudo qemu-system-x86_64 -cpu host -m 1G -nodefaults -display none -serial stdio -kernel build/nginx_kvm-x86_64 -initrd initramfs.cpio -append "-- -c /nginx/conf/nginx.conf" --enable-kvm -netdev user,id=n0,hostfwd=tcp::80-:80 -device virtio-net,netdev=n0 -s
